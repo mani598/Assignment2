bf2a703faf0cb2534d422e74ef250a40ff06f9ec
Mani <mani.nz.mani@gmail.com>
2019-10-10 21:42:55 +1300

    Hi Cheri, Exception class for you.

--- src/main/java/nz/ac/eit/Assignment2/Exception.java
+++ src/main/java/nz/ac/eit/Assignment2/Exception.java
@@ -1,20 +1,8 @@
 package nz.ac.eit.Assignment2;
 
-import static nz.ac.eit.Assignment2.Dealer.dealerThrow;
-import static nz.ac.eit.Assignment2.Player.player;
+import java.util.Scanner;
 
 public class Exception extends Game{
-    public String input(){
-        String gameInput = "";
-        if (gameInput == "y"){
-            player();
-            dealerThrow();
-        }else if (gameInput == "n"){
-            System.out.println("... Goodbye ...");
-        }else {
 
-        }
-        return gameInput;
-    }
 
 }
