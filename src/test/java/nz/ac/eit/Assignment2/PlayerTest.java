399f3865a9efdae36d90e3685e4e8210fadc614e
Mani <mani.nz.mani@gmail.com>
2019-10-11 22:50:19 +1300

    Start Player class test (playerSum more than 21 || less than 21 || equals 21).

---
+++ src/test/java/nz/ac/eit/Assignment2/PlayerTest.java
@@ -0,0 +1,19 @@
+package nz.ac.eit.Assignment2;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+    private Player player;
+
+    @Before
+    public void setup() {
+        player = new Player();
+    }
+
+    @Test
+    public void playerSum_lessThan21() {
+
+    }
+}
