cb6bd320c1b07d850c7676f606b16a3a62581523
Mani <mani.nz.mani@gmail.com>
2019-10-11 22:38:59 +1300

    class test result 70% & all of them green.

---
+++ src/test/java/nz/ac/eit/Assignment2/DiceTest.java
@@ -0,0 +1,29 @@
+package nz.ac.eit.Assignment2;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class DiceTest {
+
+    private Dice dice;
+
+    @Before
+    public void setup(){
+        dice = new Dice();
+    }
+
+    @Test
+    public void diceRoll_noLessThan1() {
+        int diceRollValue = dice.diceRoll();
+        boolean result = diceRollValue < 2;
+        assertEquals("Both die should roll between 2 and 12, die rolled less than 2 result false", false,result);
+    }
+
+    @Test
+    public void diceRoll_moreThan6() {
+        int diceRollValue = dice.diceRoll();
+        boolean result = diceRollValue > 12;
+        assertEquals("Both die should roll between 2 and 12, die rolled more than 12 result false", false, result);
+    }
+}
