62fcd20525dae8514e8aa582bca19db1b85164af
Mani <mani.nz.mani@gmail.com>
2019-10-13 12:38:29 +1300

    Hi Cheri, can you please help me with this class test.

--- src/test/java/nz/ac/eit/Assignment2/PlayerTest.java
+++ src/test/java/nz/ac/eit/Assignment2/PlayerTest.java
@@ -2,18 +2,28 @@ package nz.ac.eit.Assignment2;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Scanner;
+
+import static nz.ac.eit.Assignment2.Dealer.*;
+import static nz.ac.eit.Assignment2.Dice.*;
 import static org.junit.Assert.*;
 
 public class PlayerTest {
-    private Player player;
 
-    @Before
+    /*private Player mmm;*/
+
+
+    /*@Before
     public void setup() {
-        player = new Player();
-    }
+        mmm = new Player();
+    }*/
 
     @Test
-    public void playerSum_lessThan21() {
-
+    public void test_player_rollResult() {
+        /*Player result = new Player();
+        playerSum = diceRoll();
+        System.out.println("You started out with: " + playerSum + "\n");
+        assertEquals(result,false);*/
     }
 }
