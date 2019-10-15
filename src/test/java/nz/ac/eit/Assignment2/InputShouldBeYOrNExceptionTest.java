e5f02b36ea67dd6ed0d3fe6d7533ebf5265af5b4
Cheri Henderson <cherithemonkey@gmail.com>
2019-10-15 14:56:47 +1300

    Added test class for InputShouldBeYOrNException, not working yet so it's commented out for now.

---
+++ src/test/java/nz/ac/eit/Assignment2/InputShouldBeYOrNExceptionTest.java
@@ -0,0 +1,16 @@
+package nz.ac.eit.Assignment2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InputShouldBeYOrNExceptionTest {
+
+    @Test
+    public void getMessage() {
+//assertEquals("The input must be either 'y' or 'n'.");
+        //still not quite got my head around this, will try to get it working later. Commented out for now
+        //so it won't cause errors
+
+    }
+}
